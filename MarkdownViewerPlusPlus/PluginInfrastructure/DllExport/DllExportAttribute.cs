cfe783e9e0429dc7a9875b3a18d5a2678bd7dd6e
jms830 <jms830@users.noreply.github.com>
2026-02-02 13:48:26 -0500

    revert: restore original NppPlugin.DllExport and .NET 4.0 target
    Revert from 3F/DllExport back to the original NppPlugin.DllExport that
    shipped with the working 0.8.2 release. The 3F/DllExport was causing
    the plugin panel to not display even though the DLL loaded correctly.
    
    Key changes:
    - Restore .NET Framework 4.0 target (was 4.8)
    - Restore NppPlugin.DllExport namespace (was RGiesecke.DllExport)
    - Restore original DllExportAttribute.cs
    - Restore original UnmanagedExports.cs
    - Update CI to set DevEnvDir for DllExportTask

--- MarkdownViewerPlusPlus/PluginInfrastructure/DllExport/DllExportAttribute.cs
+++ MarkdownViewerPlusPlus/PluginInfrastructure/DllExport/DllExportAttribute.cs
@@ -1,19 +1,9 @@
-// NPP plugin platform for .Net v0.94.00 by Kasper B. Graversen etc.
-// Modified to use 3F/DllExport which fixes NaN IL assembler issues
+﻿// NPP plugin platform for .Net v0.94.00 by Kasper B. Graversen etc.
 using System;
 using System.Runtime.InteropServices;
 
-namespace RGiesecke.DllExport
+namespace NppPlugin.DllExport
 {
-    /// <summary>
-    /// The fully qualified type name must be <c>RGiesecke.DllExport.DllExportAttribute</c> in order to work with 3F/DllExport.
-    /// This local implementation avoids a runtime reference to DllExport.dll which would be copied to output.
-    /// <para>
-    /// 3F/DllExport uses a modified IL Assembler that correctly handles double.NaN values which caused
-    /// "syntax error at token '-' in: IL_0008: ldc.r8 -nan(ind)" errors with other DllExport implementations.
-    /// See <seealso href="https://github.com/3F/DllExport"/>
-    /// </para>
-    /// </summary>
     [AttributeUsage(AttributeTargets.Method, AllowMultiple = false)]
     partial class DllExportAttribute : Attribute
     {
