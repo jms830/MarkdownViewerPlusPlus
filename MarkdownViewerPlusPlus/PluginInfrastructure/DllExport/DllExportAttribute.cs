9db7a9b6a8b737bcb2b63c0a44fd55fe58fe32b0
jms830 <jms830@users.noreply.github.com>
2026-02-02 13:54:45 -0500

    fix: set DllExportOrdinalsBase to 0 to match original plugin
    The original 0.8.2 release has Ordinal Base 0 in its export table.
    Our build had Ordinal Base 1 due to 3F/DllExport default settings.
    
    This difference may be causing the plugin panel to not display even
    though the DLL loads and menu items appear.

--- MarkdownViewerPlusPlus/PluginInfrastructure/DllExport/DllExportAttribute.cs
+++ MarkdownViewerPlusPlus/PluginInfrastructure/DllExport/DllExportAttribute.cs
@@ -1,9 +1,19 @@
-﻿// NPP plugin platform for .Net v0.94.00 by Kasper B. Graversen etc.
+// NPP plugin platform for .Net v0.94.00 by Kasper B. Graversen etc.
+// Modified to use 3F/DllExport which fixes NaN IL assembler issues
 using System;
 using System.Runtime.InteropServices;
 
-namespace NppPlugin.DllExport
+namespace RGiesecke.DllExport
 {
+    /// <summary>
+    /// The fully qualified type name must be <c>RGiesecke.DllExport.DllExportAttribute</c> in order to work with 3F/DllExport.
+    /// This local implementation avoids a runtime reference to DllExport.dll which would be copied to output.
+    /// <para>
+    /// 3F/DllExport uses a modified IL Assembler that correctly handles double.NaN values which caused
+    /// "syntax error at token '-' in: IL_0008: ldc.r8 -nan(ind)" errors with other DllExport implementations.
+    /// See <seealso href="https://github.com/3F/DllExport"/>
+    /// </para>
+    /// </summary>
     [AttributeUsage(AttributeTargets.Method, AllowMultiple = false)]
     partial class DllExportAttribute : Attribute
     {
